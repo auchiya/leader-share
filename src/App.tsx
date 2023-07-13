7a34b66d88b6e3252284b7ab78e31b02b742b72d
Arkaitz Vizuete <code@arkaitzvizuete.com>
2023-07-13 14:31:22 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
-import Leaderboard from "./Leaderboard";
-import { leaderboards } from "./data";
 import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Profile } from "./Profile";
+import { LeaderBoardPage } from "./LeaderBoardPage";
 
 const App = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
-
-  const handleTabChange = (newValue: number) => {
-    setSelectedTab(newValue);
-  };
+  
 
   return (
-    <div>
-      <h1>Leaderboards</h1>
-      <ul className="nav nav-tabs">
-        {leaderboards.map((leaderboard, index) => (
-          <li className="nav-item" key={index}>
-            <a
-              className={`nav-link ${selectedTab === index ? "active" : ""}`}
-              onClick={() => handleTabChange(index)}
-            >
-              {leaderboard.name}
-            </a>
-          </li>
-        ))}
-      </ul>
-      {leaderboards.map((leaderboard, index) => (
-        <div key={index} hidden={selectedTab !== index}>
-          {selectedTab === index && (
-            <Leaderboard players={leaderboard.players} />
-          )}
-        </div>
-      ))}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LeaderBoardPage />} />
+        <Route path="/profile" element={<Profile/>} />
+      </Routes>
+
+    </BrowserRouter>
   );
 };
 
