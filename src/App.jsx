389c5357facc86d7f3e66e628de06d886e28c87e
auchiya <uchiya.agustin@gmail.com>
2023-07-13 11:38:26 +0200

    more players

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import Leaderboard from "./Leaderboard";
 import { players } from "./Player";
 
 function App() {
+  // Generate 50 players
+  for (let i = 3; i <= 50; i++) {
+    players.push({
+      name: `Player ${i}`,
+      nickname: `P${i}`,
+      score: Math.floor(Math.random() * 1000), // Random score between 0 and 1000
+    });
+  }
+
   const sortedPlayers = players.sort((a, b) => b.score - a.score);
 
   return (
--- src/Player.ts
+++ src/Player.ts
@@ -1,10 +1,11 @@
 export type Player = {
   name: string;
+  nickname: string;
   score: number;
 };
 
 export const players: Player[] = [
-  { name: "Player 1", score: 100 },
-  { name: "Player 2", score: 200 },
+  { name: "Player 1", nickname: "P1", score: 100 },
+  { name: "Player 2", nickname: "P2", score: 200 },
   // ...
 ];
